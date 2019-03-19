7612f7df302598b68631498db0987bf9f7c5ddf1
ryanml <rlanese@asu.edu>
2019-03-19 01:49:07 -0700

    Fixes brave/brave-browser#2245
    Fixes brave/brave-browser#2823
    
    Monthly donation functionality via panel

--- components/brave_rewards/resources/extension/brave_rewards/background/reducers/rewards_panel_reducer.ts
+++ components/brave_rewards/resources/extension/brave_rewards/background/reducers/rewards_panel_reducer.ts
@@ -113,6 +113,7 @@ export const rewardsPanelReducer = (state: RewardsExtension.State | undefined, a
         delete publishers[id]
       } else {
         publishers[id] = { ...publishers[id], ...publisher }
+        chrome.braveRewards.getPublisherDonationAmounts(publisher.publisher_key)
       }
 
       state = {
@@ -303,6 +304,36 @@ export const rewardsPanelReducer = (state: RewardsExtension.State | undefined, a
         state[key] = value
         chrome.braveRewards.saveSetting(key, value)
       }
+    }
+    case types.REMOVE_RECURRING_DONATION: {
+      let publisherKey = payload.publisherKey
+      if (publisherKey == null) {
+        break
+      }
+      chrome.braveRewards.removeRecurringDonation(publisherKey)
+      break
+    }
+    case types.SAVE_RECURRING_DONATION: {
+      let newAmount = payload.newAmount
+      let publisherKey = payload.publisherKey
+
+      if (newAmount < 0 ||
+          isNaN(newAmount) ||
+          publisherKey == null) {
+        break
+      }
+
+      chrome.braveRewards.saveRecurringDonation(publisherKey, newAmount)
+      break
+    }
+    case types.ON_RECURRING_DONATIONS: {
+      state = { ...state }
+      state.recurringDonations = payload.result.recurringDonations
+      break
+    }
+    case types.ON_PUBLISHER_DONATION_AMOUNTS: {
+      state = { ...state }
+      state.donationAmounts = payload.amounts
       break
     }
   }
